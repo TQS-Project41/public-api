ef1856e02b1b572e7775c1b11c65cfd1d7674ef3
Pedro Duarte <pedro.dld536@gmail.com>
2022-06-11 20:52:02 +0100

    Business registration

---
+++ project/src/main/java/com/tqs/project/dto/BusinessDto.java
@@ -0,0 +1,17 @@
+package com.tqs.project.dto;
+
+public class BusinessDto {
+
+  private String email;
+
+  private String password;
+
+  public String getEmail() {
+    return this.email;
+  }
+
+  public String getPassword() {
+    return this.password;
+  }
+  
+}
